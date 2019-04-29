7d5122a6d1bad017cd459767fdeebd38f37f5089
Karol Klimonczyk <k.klimonczyk@gmail.com>
2019-04-29 20:19:09 +0200

    bonsai.io client

--- kafka-consumer-elasticsearch/src/main/java/com/jvmfy/kafka/ElasticSearchConsumer.java
+++ kafka-consumer-elasticsearch/src/main/java/com/jvmfy/kafka/ElasticSearchConsumer.java
@@ -1,5 +1,52 @@
 package com.jvmfy.kafka;
 
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
 public class ElasticSearchConsumer {
 
+    public static void main(String[] args) throws IOException {
+        Logger log = LoggerFactory.getLogger(ElasticSearchConsumer.class);
+        RestHighLevelClient client = createClient();
+
+        String jsonString = "{\"foo\": \"bar\"}";
+
+        IndexRequest indexRequest = new IndexRequest(
+                "twitter",
+                "tweets"
+        ).source(jsonString, XContentType.JSON);
+
+        IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
+
+        String id = indexResponse.getId();
+        log.info(id);
+        client.close();
+    }
+
+    private static RestHighLevelClient createClient() {
+
+
+        final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
+
+        RestClientBuilder builder = RestClient.builder(
+                new HttpHost(hostname, 443, "https"))
+                .setHttpClientConfigCallback(httpClientBuilder -> httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider));
+
+        return new RestHighLevelClient(builder);
+    }
 }
